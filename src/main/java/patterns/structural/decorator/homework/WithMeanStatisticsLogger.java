a1120273a533c014f39fab642c50b8a489f54b35
Boriss Harin <boriss.harin@gmail.com>
2022-11-08 11:52:40 +0200

    Decorator homework

---
+++ src/main/java/patterns/structural/decorator/homework/WithMeanStatisticsLogger.java
@@ -0,0 +1,27 @@
+package patterns.structural.decorator.homework;
+
+public class WithMeanStatisticsLogger implements StatisticsLogger{
+
+    private final StatisticsLogger statisticsLogger;
+
+    public WithMeanStatisticsLogger(StatisticsLogger statisticsLogger) {
+        this.statisticsLogger = statisticsLogger;
+    }
+
+    @Override
+    public String buildLog() {
+        return getMeanStatistic(statisticsLogger) + statisticsLogger.buildLog();
+    }
+
+    String getMeanStatistic(StatisticsLogger statisticsLogger) {
+        String[] dataString = statisticsLogger.buildLog().split("BaseStatistics:\n");
+        String[] integers = dataString[1].split("\n");
+
+        int sum = 0;
+        for (String s: integers) {
+            sum += Integer.parseInt(s);
+        }
+        String log = "Mean:\n";
+        return log + sum / integers.length + "\n";
+    }
+}
