7526c846acfab33027dd260175ef7150a84fe484
Boriss Harin <boriss.harin@gmail.com>
2022-11-11 09:00:55 +0200

    homework removed

--- src/main/java/patterns/behavioral/observer/homework/Observer.java
+++
@@ -1,6 +0,0 @@
-package patterns.behavioral.observer.homework;
-
-public abstract class Observer {
-    Subject subject;
-    abstract void update();
-}
--- src/main/java/patterns/structural/decorator/homework/StatisticsLogger.java
+++
@@ -1,5 +0,0 @@
-package patterns.structural.decorator.homework;
-
-public interface StatisticsLogger {
-    String buildLog();
-}
