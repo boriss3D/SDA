a2ec302cef56b7e4c9d60d423f87d7ce1b4ffd0a
Boriss Harin <boriss.harin@gmail.com>
2022-11-22 18:45:18 +0200

    Task03

---
+++ src/main/java/advanced/exercise/ParallelAndConcurrent/Task03/Task03.java
@@ -0,0 +1,19 @@
+package advanced.exercise.ParallelAndConcurrent.Task03;
+
+import java.util.Random;
+
+public class Task03 {
+    public static void main(String[] args) throws InterruptedException {
+        int[] ints = new int[50000];
+        int[] intCopy = new int[50000];
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = new Random().nextInt();
+        }
+
+        System.arraycopy(ints, 0, intCopy, 0, ints.length);
+
+        new Thread(() -> BubbleAlgorithm.sort(ints)).start();
+        new Thread(() -> SelectionSortAlgorithm.insertionSort(intCopy)).start();
+    }
+}
